9abba8454d8966ae271e46da95c7f006b4400d1d
Eben's <mac@MacBook-Air.local>
2026-04-16 21:45:59 +0100

    phone-number

--- dist/model/BookingModel.js
+++ dist/model/BookingModel.js
@@ -44,7 +44,7 @@ const BookingSchema = new mongoose_1.Schema({
         type: String,
         required: [true, 'Contact number is required'],
         trim: true,
-        // match: [/^[\+]?[1-9][\d]{0,15}$/, 'Please enter a valid contact number']
+        match: [/^[\+]?[0-9][\d]{7,20}$/, 'Please enter a valid contact number']
     },
     proposedDate: {
         type: Date,
--- src/model/BookingModel.ts
+++ src/model/BookingModel.ts
@@ -36,7 +36,7 @@ const BookingSchema: Schema = new Schema(
             type: String,
             required: [true, 'Contact number is required'],
             trim: true,
-            // match: [/^[\+]?[1-9][\d]{0,15}$/, 'Please enter a valid contact number']
+            match: [/^[\+]?[0-9][\d]{7,20}$/, 'Please enter a valid contact number']
         },
         proposedDate: {
             type: Date,
